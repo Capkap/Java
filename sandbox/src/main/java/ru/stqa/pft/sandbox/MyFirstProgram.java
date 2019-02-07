f8a2cc813af8367152c350d3fd871894b9cd2977
sakhmedov <s.akhmedov@mfisoft.ru>
2019-02-07 17:11:28 +0300

    Made distance function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        hello("world");
-        hello("Sam");
+
+        Point p1 = new Point(2,4);
+        Point p2 = new Point(5,7);
+        System.out.println("Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + distance(p1,p2));
 
         Square s = new Square(5);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
@@ -13,7 +15,7 @@ public class MyFirstProgram {
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
+    public static double distance(Point p1, Point p2) {
+      return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y - p1.y,2));
     }
 }
